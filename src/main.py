aaf75e593c8f85f482cbfffcd91e1c44917570d3
Tanmay Zade <tanmayzade2002@gmail.com>
2025-11-15 13:40:50 +0000

    Refactor comment for clarity

--- src/main.py
+++ src/main.py
@@ -255,3 +255,4 @@
 # Update: Improve example usage (2025-11-09 13:40:41 UTC)
 # Update: Fix typo in code comment (2025-11-11 13:47:44 UTC)
 # Update: Update sample data note (2025-11-14 13:46:25 UTC)
+# Update: Refactor comment for clarity (2025-11-15 13:40:50 UTC)
